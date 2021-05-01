5693e27b1855f5d63e9d4d830da4a34a7290a2e8
Z572 <873216071@qq.com>
2021-05-01 23:07:55 +0800

    202105012307

--- Z572/rde.scm
+++ Z572/rde.scm
@@ -2,19 +2,73 @@
 (use-modules (guix) (gnu)
              (srfi srfi-26)
              (guix transformations))
-(use-package-modules linux)
+(use-package-modules linux emacs-xyz)
 (use-system-modules keyboard)
 (use-modules (gnu home)
+             (guix git-download)
+             (guix build-system emacs)
              (gnu home-services)
              (gnu home-services gnupg)
              (gnu home-services state)
              (gnu home-services mcron)
+             (gnu home-services password-utils)
              (gnu home-services shells)
              (gnu home-services shellutils)
              (gnu home-services files)
              (gnu home-services ssh)
              (gnu home-services emacs)
              (gnu home-services version-control))
+
+(define emacs-leaf-keywords
+  (package
+    (name "emacs-leaf-keywords")
+    (version "20210222.1243")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (string-append
+             "https://melpa.org/packages/leaf-keywords-"
+             version
+             ".tar"))
+       (sha256
+        (base32
+         "1g4d5h9kbsbnq4pp9q36fcpc4v89k7xcnwyhi1mca2v3kan84144"))))
+    (build-system emacs-build-system)
+    (propagated-inputs `(("emacs-leaf" ,emacs-leaf)))
+    (home-page
+     "https://github.com/conao3/leaf-keywords.el")
+    (synopsis
+     "Additional leaf.el keywords for external packages")
+    (description
+     "leaf-keywords.el provide additional keywords for leaf.el
+This package defines keywords that are dependent on an external package.
+
+More information is [[https://github.com/conao3/leaf-keywords.el][here]]
+")
+    (license #f)))
+
+;; (define-public emacs-straight
+;;   (package
+;;     (name "emacs-straight")
+;;     (version "33dbc6")
+;;     (source
+;;      (origin
+;;        (method git-fetch)
+;;        (uri (git-reference
+;;              (url "https://github.com/raxod502/straight.el")
+;;              (commit "33dbc6eac2a46462f6ec466a053144c1850b84d6")
+;;              ))
+;;        (sha256
+;;         (base32
+;;          "1kcq0z7f8474320w120rlb8dkqj5pk66bjhk6yi0c5h6wyx5kkz6"))
+;;        (file-name (git-file-name name version))))
+;;     (inputs `(("git" ,git-minimal)))
+;;     (build-system emacs-build-system)
+;;     (home-page
+;;      "https://github.com/raxod502/straight.el")
+;;     (synopsis "")
+;;     (description "")
+;;     (license #f)))
 
 (define sample-he
   (home-environment
@@ -31,7 +85,6 @@
           '(python-language-server
             python
             guile-hall
-            krita
             fontmanager
             fzf
             sbcl
@@ -39,7 +92,6 @@
             okular
             tree
             translate-shell
-            direnv
             telegram-desktop
             tintin++
             isync
@@ -55,17 +107,21 @@
             zip
             tidy
             gettext
-            guile-hall
             picom
             inkscape
             ccls))))
    (services
     (list
      (simple-service
-      'my-env home-environment-vars-service-type
-      '(("GUIX_GTK3_IM_MODULE_FILE" . "/run/current-system/profile/lib/gtk-3.0/3.0.0/immodules-gtk3.cache")
+      'my-env home-environment-variables-service-type
+      '(("QT_IM_MODULE" . "ibus")
+        ("GTK_IM_MODULE" . "ibus")
+        ("XMODIFIERS" . "@im=ibus")
+        ("GUIX_GTK3_IM_MODULE_FILE" . "/run/current-system/profile/lib/gtk-3.0/3.0.0/immodules-gtk3.cache")
         ("GUIX_GTK2_IM_MODULE_FILE" . "/run/current-system/profile/lib/gtk-2.0/2.10.0/immodules-gtk2.cache")
-        ("GUIX_GITHUB_TOKEN" . "c108c863fe50516c1af2be509d0e8d04ddb3fbfe")))
+        ("GUIX_GITHUB_TOKEN" . "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"
+         ;; "c108c863fe50516c1af2be509d0e8d04ddb3fbfe"
+         )))
      (simple-service
       'set-brightness-on-login home-run-on-first-login-service-type
       #~(system* #$(file-append light "/bin/light") "-S" "90"))
@@ -108,62 +164,86 @@
                   (github ((user . "Z572")))
                   (gitlab ((user . "Z572")))))))
      (service home-zsh-autosuggestions-service-type)
+     (service (service-type
+               (name 'home-bash-direnv)
+               (extensions
+                (list
+                 (service-extension
+                  home-profile-service-type
+                  (const (list (specification->package "direnv"))))
+                 (service-extension
+                  home-bash-service-type
+                  (const (home-bash-extension
+                          (bashrc (list "eval \"$(direnv hook bash)\"")))))))
+               (default-value #f)
+               (description "Enables @code{direnv} for @code{bash}.  Adds hook to
+@file{.bashrc} and installs a package in the profile.")))
      (service home-zsh-direnv-service-type)
+     (service home-bash-service-type
+              (home-bash-configuration
+               (guix-defaults? #t)
+               (bashrc '("\
+shopt -s autocd
+shopt -s checkwinsize
+"))))
      (service home-emacs-service-type
               (home-emacs-configuration
-               (package (specification->package+output "emacs-native-comp"))
+               (package (specification->package+output "emacs-next"))
                (server-mode? #t)
-               (elisp-packages (map (compose specification->package+output symbol->string)
-                                    '(emacs-all-the-icons
-                                      emacs-avy
-                                      emacs-blackout
-                                      emacs-cider
-                                      emacs-cmake-mode
-                                      emacs-company-box
-                                      emacs-ctrlf
-                                      emacs-haskell-mode
-                                      emacs-debbugs
-                                      emacs-dimmer
-                                      emacs-diredfl
-                                      emacs-doom-modeline
-                                      emacs-eglot
-                                      emacs-emojify
-                                      emacs-envrc
-                                      emacs-flycheck
-                                      emacs-flycheck-guile
-                                      emacs-go-mode
-                                      emacs-guix
-                                      emacs-helpful
-                                      emacs-highlight-defined
-                                      emacs-hl-todo
-                                      emacs-macrostep
-                                      emacs-magit
-                                      emacs-magit-todos
-                                      emacs-next
-                                      emacs-nix-mode
-                                      emacs-no-littering
-                                      emacs-nov-el
-                                      emacs-olivetti
-                                      emacs-page-break-lines
-                                      emacs-paren-face
-                                      emacs-pdf-tools
-                                      emacs-prescient
-                                      emacs-repology
-                                      emacs-restart-emacs
-                                      emacs-rime
-                                      emacs-rust-mode
-                                      emacs-selectrum
-                                      emacs-shackle
-                                      emacs-sly
-                                      emacs-symbol-overlay
-                                      emacs-telega
-                                      emacs-vterm
-                                      emacs-web-mode
-                                      emacs-which-key
-                                      emacs-ws-butler
-                                      emacs-yasnippet
-                                      emacs-yasnippet-snippets
-                                      emacs-aggressive-indent)))))
+               (elisp-packages
+                (list emacs-all-the-icons
+                      emacs-leaf-keywords
+                      emacs-avy
+                      emacs-blackout
+                      emacs-cider
+                      ;;emacs-straight
+                                        ;emacs-cmake-mode
+                      emacs-company-box
+                      emacs-ctrlf
+                      emacs-haskell-mode
+                      emacs-leaf
+                      emacs-debbugs
+                      emacs-dimmer
+                      emacs-diredfl
+                      emacs-doom-modeline
+                      emacs-eglot
+                      emacs-emojify
+                      emacs-envrc
+                      emacs-flycheck
+                      emacs-flycheck-guile
+                      emacs-go-mode
+                      emacs-guix
+                      emacs-helpful
+                      emacs-highlight-defined
+                      emacs-hl-todo
+                      emacs-macrostep
+                      emacs-magit
+                      emacs-magit-todos
+                      emacs-nix-mode
+                      emacs-no-littering
+                      emacs-nov-el
+                      emacs-olivetti
+                      emacs-page-break-lines
+                      emacs-paren-face
+                      emacs-pdf-tools
+                      emacs-prescient
+                      emacs-repology
+                      emacs-leetcode
+                      emacs-restart-emacs
+                      emacs-rime
+                      emacs-rust-mode
+                      emacs-selectrum
+                      emacs-shackle
+                      emacs-sly
+                      emacs-symbol-overlay
+                      emacs-telega
+                      emacs-vterm
+                      emacs-web-mode
+                      emacs-which-key
+                      emacs-ws-butler
+                      emacs-yasnippet
+                      emacs-yasnippet-snippets
+                      emacs-aggressive-indent))))
      (service home-zsh-service-type
               (home-zsh-configuration
                (xdg-flavor? #t)
@@ -193,6 +273,9 @@ SAVEHIST=10000
                  (pinentry-flavor 'emacs)
                  (extra-options '("--verbose"))
                  (extra-config
-                  '((max-cache-ttl . 86400)))))))))))
+                  '((max-cache-ttl . 86400)))))))
+     (service home-password-store-service-type
+              (home-password-store-configuration
+               (browserpass-native? #t)))))))
 
 sample-he
