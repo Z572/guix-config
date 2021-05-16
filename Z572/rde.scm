d9989fc6cbbfcca7e254c63eb61efa4ddb7a5a63
Z572 <873216071@qq.com>
2021-05-16 23:36:21 +0800

    202105162336

--- Z572/rde.scm
+++ Z572/rde.scm
@@ -1,10 +1,14 @@
 ;; -*- mode: scheme -*-
+(add-to-load-path (format #f "~a/.config/guix/config/" (getenv "HOME")))
 (use-modules (guix) (gnu)
              (srfi srfi-26)
+             (ice-9 format)
              (guix transformations))
 (use-package-modules linux emacs-xyz)
 (use-system-modules keyboard)
-(use-modules (gnu home)
+(use-modules (Z572)
+             (gnu home)
+             ((guix licenses) #:prefix license:)
              (guix git-download)
              (guix build-system emacs)
              (gnu home-services)
@@ -18,6 +22,14 @@
              (gnu home-services ssh)
              (gnu home-services emacs)
              (gnu home-services version-control))
+
+;; Add Intimate information
+
+;; (save-module-excursion
+;;  (lambda () (primitive-load (format #f "~a/.config/guix/config/Z572.scm" (getenv "HOME"))
+;;                             ;; (string-append (dirname (current-filename)) "/../Z572.scm")
+;;                             )))
+
 
 (define-public z-emacs-rime
   (let ((commit "519e6eb3b5e8e668c2835d27f54fcf5776242576")
@@ -34,51 +46,87 @@
                (commit commit)))
          (file-name (git-file-name name version))
          (sha256
-          (base32 "1q0kzdy3nxswsriq4fxr00wmw43x737dd7pnkf9g6v8hd7gsqkzc"))))
-      (build-system emacs-build-system)
-      (arguments
-       '(#:include (cons "\\.so$" %default-include)
-         #:phases
-         (modify-phases %standard-phases
-           (add-after 'unpack 'patch-rime-data-path
-             (lambda* (#:key inputs #:allow-other-keys)
-               (make-file-writable "rime.el")
-               (emacs-substitute-variables "rime.el"
-                 ("rime-share-data-dir"
-                  (string-append (assoc-ref inputs "rime-data")
-                                 "/share/rime-data")))
-               #t))
-           (add-before 'install 'build-emacs-module
-             (lambda _
-               (invoke "make" "lib")))))))))
+          (base32 "1q0kzdy3nxswsriq4fxr00wmw43x737dd7pnkf9g6v8hd7gsqkzc")))))))
 
-(define emacs-leaf-keywords
+(define-public emacs-leaf-keywords
+  ;;latest git tag is on May 29, 2019, 1.1.0, but leaf-keywords.el version is
+  ;;2.0.5
+  (let ((commit "4146621f4ae80ef0c30160337119441c1f6334b6")
+        (revision "0"))
+    (package
+      (name "emacs-leaf-keywords")
+      (version (git-version "2.0.5" revision commit))
+      (source
+       (origin
+         (method git-fetch)
+         (uri (git-reference
+               (url "https://github.com/conao3/leaf-keywords.el")
+               (commit commit)))
+         (file-name (git-file-name name version))
+         (sha256
+          (base32
+           "16iv1cvlky2gij1ndx2d6q8l35axm72bx52n6v5y3h21aibj197n"))))
+      (build-system emacs-build-system)
+      (propagated-inputs `(("emacs-leaf" ,emacs-leaf)))
+      (home-page
+       "https://github.com/conao3/leaf-keywords.el")
+      (synopsis
+       "Additional leaf.el keywords for external packages")
+      (description
+       "@code{leaf-keywords} provide additional keywords for leaf.el defines
+keywords that are dependent on an external package.")
+      (license license:gpl3+))))
+
+(define-public emacs-easy-escape
   (package
-    (name "emacs-leaf-keywords")
-    (version "20210222.1243")
+    (name "emacs-easy-escape")
+    (version "20161209.1544")
     (source
      (origin
        (method url-fetch)
        (uri (string-append
-             "https://melpa.org/packages/leaf-keywords-"
+             "https://melpa.org/packages/easy-escape-"
              version
-             ".tar"))
+             ".el"))
        (sha256
         (base32
-         "1g4d5h9kbsbnq4pp9q36fcpc4v89k7xcnwyhi1mca2v3kan84144"))))
+         "1z9xc3w5l2v2mhrncqsbbkax2jbd83p812svjhfr7zdlidd9v9l6"))))
     (build-system emacs-build-system)
-    (propagated-inputs `(("emacs-leaf" ,emacs-leaf)))
     (home-page
-     "https://github.com/conao3/leaf-keywords.el")
+     "https://github.com/cpitclaudel/easy-escape")
     (synopsis
-     "Additional leaf.el keywords for external packages")
+     "Improve readability of escape characters in regular expressions")
     (description
-     "leaf-keywords.el provide additional keywords for leaf.el
-This package defines keywords that are dependent on an external package.
+     "`easy-escape-minor-mode' uses syntax highlighting and composition to make ELisp regular
+expressions more readable.  More precisely, it hides double backslashes
+preceding regexp specials (`()|'), composes other double backslashes into
+single ones, and applies a special face to each.  The underlying buffer text
+is not modified.
 
-More information is [[https://github.com/conao3/leaf-keywords.el][here]]
+For example, `easy-escape` prettifies this:
+  \"\\\\(?:\\\\_<\\\\\\\
+ewcommand\\\\_>\\\\s-*\\\\)?\"
+into this (`^' indicates a different color):
+  \"(?:\\_<\\\
+ewcommand\\_>\\s-*)?\".
+   ^                        ^
+
+The default is to use a single \\ character instead of two, and to hide
+backslashes preceding parentheses or `|'.  The escape character and its color
+can be customized using `easy-escape-face' and `easy-escape-character' (which see), and backslashes
+before ()| can be shown by disabling `easy-escape-hide-escapes-before-delimiters'.
+
+Suggested setup:
+  (add-hook 'lisp-mode-hook 'easy-escape-minor-mode)
+
+NOTE: If you find the distinction between the fontified double-slash and the
+single slash too subtle, try the following:
+
+* Adjust the foreground of `easy-escape-face'
+* Set `easy-escape-character' to a different character.
 ")
     (license #f)))
+
 
 (define emacs-winum
   (package
@@ -135,13 +183,38 @@ frames, giving the user a smoother experience of multi-screen Emacs.
 ;;     (description "")
 ;;     (license #f)))
 
+(define-public emacs-meow
+  (package
+    (name "emacs-meow")
+    (version "20210511.314")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (string-append
+             "https://melpa.org/packages/meow-"
+             version
+             ".tar"))
+       (sha256
+        (base32
+         "0jmb5vwqv7vmnlq1lpc6676z69533rzs6xgly30079la4bh8mi4z"))))
+    (build-system emacs-build-system)
+    (propagated-inputs
+     `(("emacs-dash" ,emacs-dash) ("emacs-s" ,emacs-s)))
+    (home-page
+     "https://www.github.com/DogLooksGood/meow")
+    (synopsis "Modal Editing On Wheel")
+    (description
+     "Enable `meow-global-mode' to activate modal editing.
+")
+    (license #f)))
+
 (define sample-he
   (home-environment
-   (keyboard-layout
-    (keyboard-layout
-     "us" #:options
-     '("ctrl:nocaps"
-       "shift:both_capslock")))
+   ;; (keyboard-layout
+   ;;  (keyboard-layout
+   ;;   "us" #:options
+   ;;   '("ctrl:nocaps"
+   ;;     "shift:both_capslock")))
    (home-directory (getenv "HOME"))
    (packages
     (append
@@ -150,6 +223,7 @@ frames, giving the user a smoother experience of multi-screen Emacs.
           '(python-language-server
             python
             guile-hall
+            ;;guile-bash
             fontmanager
             fzf
             sbcl
@@ -179,17 +253,52 @@ frames, giving the user a smoother experience of multi-screen Emacs.
     (list
      (simple-service
       'my-env home-environment-variables-service-type
-      '(("QT_IM_MODULE" . "ibus")
+      `(("QT_IM_MODULE" . "ibus")
         ("GTK_IM_MODULE" . "ibus")
         ("XMODIFIERS" . "@im=ibus")
         ("GUIX_GTK3_IM_MODULE_FILE" . "/run/current-system/profile/lib/gtk-3.0/3.0.0/immodules-gtk3.cache")
         ("GUIX_GTK2_IM_MODULE_FILE" . "/run/current-system/profile/lib/gtk-2.0/2.10.0/immodules-gtk2.cache")
-        ("GUIX_GITHUB_TOKEN" . "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"
+        ("GUIX_GITHUB_TOKEN" . ,z-guix-github-token
          ;; "c108c863fe50516c1af2be509d0e8d04ddb3fbfe"
          )))
      (simple-service
       'set-brightness-on-login home-run-on-first-login-service-type
       #~(system* #$(file-append light "/bin/light") "-S" "90"))
+     (simple-service 'inputrc-config home-files-service-type
+                     (list `("inputrc" ,(plain-file
+                                         "inputrc"
+                                         (format #f "~@{~?\n~}"
+                                                 "~:@{~a: ~a\n~}"
+                                                 '(("C-p" history-search-backward)
+                                                   ("C-n" history-search-forward)
+                                                   ("M-p" history-search-backward)
+                                                   ("M-n" history-search-forward)
+                                                   ("C-a" beginning-of-line)
+                                                   ("Tab" menu-complete)
+                                                   ;;("C-e" eng-of-line)
+                                                   ("\\e[5~" beginning-of-history)
+                                                   ("\\e[6~" end-of-history)
+                                                   ("\\e[3~" delete-char)
+                                                   ("\\e[2~" quoted-insert))
+                                                 "~:@{set ~a ~:[Off~;On~]~%~}"
+                                                 '((colored-stats #t)
+                                                   (colored-completion-prefix #t)
+                                                   (convert-meta #f)
+                                                   (meta-flag #t)
+                                                   (expand-tilde #t)
+                                                   (input-meta #t)
+                                                   (output-meta #t)
+                                                   (completion-ignore-case #t)
+                                                   ;;(completion-prefix-display-length 3)
+                                                   (revert-all-at-newline #t)
+                                                   (menu-complete-display-prefix #t)
+                                                   (completion-map-case #t)
+                                                   (bind-tty-special-chars #f)
+                                                   (mark-symlinked-directories #t)
+                                                   (skip-completed-text #t)
+                                                   (show-all-if-ambiguous #t)
+                                                   (show-all-if-unmodified #t)
+                                                   (visible-stats #t)))))))
      (simple-service
       'translate-shell-config home-files-service-type
       (list `("config/translate-shell/init.trans"
@@ -223,11 +332,11 @@ frames, giving the user a smoother experience of multi-screen Emacs.
                (ignore '(".envrc"))
                (config
                 `((pull ((rebase . #t)))
-                  (user ((name . "Z572")
-                         (email . "873216071@qq.com")))
+                  (user ((name . ,z-name)
+                         (email . ,z-email)))
                   (gpg ((program . ,(file-append (specification->package "gnupg") "/bin/gpg"))))
-                  (github ((user . "Z572")))
-                  (gitlab ((user . "Z572")))))))
+                  (github ((user . ,z-name)))
+                  (gitlab ((user . ,z-name)))))))
      (service home-zsh-autosuggestions-service-type)
      (service (service-type
                (name 'home-bash-direnv)
@@ -239,10 +348,29 @@ frames, giving the user a smoother experience of multi-screen Emacs.
                  (service-extension
                   home-bash-service-type
                   (const (home-bash-extension
-                          (bashrc (list "eval \"$(direnv hook bash)\"")))))))
+                          (bashrc (list #~(format #f "eval \"$(~a hook bash)\""
+                                                  #$(file-append (specification->package "direnv") "/bin/direnv" )))))))))
                (default-value #f)
                (description "Enables @code{direnv} for @code{bash}.  Adds hook to
 @file{.bashrc} and installs a package in the profile.")))
+     ;; (service (service-type
+     ;;                (name 'home-bash-guile-bash)
+     ;;                (extensions
+     ;;                 (list
+     ;;                  (service-extension
+     ;;                   home-profile-service-type
+     ;;                   (const (list (specification->package "guile-bash"))))
+     ;;                  (service-extension
+     ;;                   home-bash-service-type
+     ;;                   (const (home-bash-extension
+     ;;                           (bashrc (list #~(string-append "enable -f " #$(file-append (specification->package "guile-bash") "/lib/bash/libguile-bash.so" )
+     ;;                                                          " scm
+     ;; "
+     ;;                                                          "GUILE_AUTO_COMPILE=0 GUILE_LOAD_PATH=$GUILE_LOAD_PATH:/home/x/.guix-home-environment/profile/share/guile/site/3.0 "
+     ;;                                                          "builtin scm \"$HOME\"/.bash.d/bash.scm"))))))))
+     ;;                (default-value #f)
+     ;;                (description "Enables @code{direnv} for @code{bash}.  Adds hook to
+     ;; @file{.bashrc} and installs a package in the profile.")))
      (service home-zsh-direnv-service-type)
      (service home-bash-service-type
               (home-bash-configuration
@@ -268,7 +396,9 @@ shopt -s checkwinsize
                       emacs-haskell-mode
                       emacs-leaf
                       emacs-debbugs
+                      emacs-meow
                       emacs-dimmer
+                      emacs-easy-escape
                       emacs-diredfl
                       emacs-doom-modeline
                       emacs-eglot
@@ -282,6 +412,7 @@ shopt -s checkwinsize
                       emacs-highlight-defined
                       emacs-hl-todo
                       emacs-macrostep
+                      emacs-forge
                       emacs-magit
                       emacs-magit-todos
                       emacs-nix-mode
@@ -293,7 +424,9 @@ shopt -s checkwinsize
                       emacs-pdf-tools
                       emacs-prescient
                       emacs-repology
+                      emacs-winum
                       emacs-leetcode
+                      emacs-ebdb
                       emacs-restart-emacs
                       z-emacs-rime
                       emacs-rust-mode
